f5dd8360fc53cd54dae8bd66b7100fbdc4087d45
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-18 14:37:49 -0700

    convert to es2015 with lebab.sh

---
+++ 02/vis.js
@@ -0,0 +1,73 @@
+    
+    // A component that renders the count.
+    const countDisplay = d3.component("div", "count-display")
+      .render(function (d){
+        d3.select(this).text(d.count); 
+      });
+    
+    // A generic Button component.
+    const button = d3.component("button")
+      .render(function (d){
+        d3.select(this)
+            .text(d.text)
+            .on("click", d.onClick);
+      });
+    
+    // The panel that contains the two buttons.
+    const buttonPanel = d3.component("div", "button-panel")
+      .render(function (d){
+        d3.select(this).call(button, [
+          {
+            text: "+",
+            onClick: d.actions.increment
+          },{
+            text: "-",
+            onClick: d.actions.decrement
+          },
+        ]);
+      });
+    
+    // The top-level app component.
+    const app = d3.component("div")
+      .render(function (d){
+        d3.select(this)
+            .call(countDisplay, d)
+            .call(buttonPanel, d);
+      });
+    
+    
+    function main(){
+      const store = Redux.createStore(reducer);
+      const actions = actionsFromDispatch(store.dispatch);
+
+      render();
+      store.subscribe(render);
+
+      function reducer (state, action){
+        const count = state || 0;
+        switch (action.type) {
+          case "INCREMENT": return count + 1;
+          case "DECREMENT": return count - 1;
+          default: return count;
+        }
+      }
+
+      function actionsFromDispatch(dispatch){
+        return {
+          increment() {
+            dispatch({ type: "INCREMENT" });
+          },
+          decrement() {
+            dispatch({ type: "DECREMENT" });
+          }
+        }
+      }
+
+      function render(){
+        d3.select("body").call(app, {
+          count: store.getState(),
+          actions
+        });
+      }
+    }
+    main();
